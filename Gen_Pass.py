b4c58216a68362dc809d6ae3cea6b5a12d23f313
FedeFari08 <fede.farinelli84@gmail.com>
2024-04-09 19:45:02 +0200

    Gen_Pass1.0

--- Gen_Pass.py
+++ Gen_Pass.py
@@ -1,9 +1,13 @@
-print ("hello world")
+import random
+caratteri = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+x = int(input("Inserisci la lungezza della password: 6"))
 
+password = ""
 
+for i in range(x):
+    password = password+random.choice(caratteri)
 
-
-
+print (password)
 
 
 
